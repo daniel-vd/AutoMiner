49a25d483209091a5fd16743ce7b5caf31186598
daniel-vd <vandijkendaniel@hotmail.com>
2017-09-04 20:54:47 +0200

    Add horizontal miner structure

--- src/com/Danielvd/AutoMiner/mining/LootStorage.java
+++ src/com/Danielvd/AutoMiner/mining/LootStorage.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -12,6 +13,9 @@ import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import de.robotricker.transportpipes.api.PipeAPI;
+import de.robotricker.transportpipes.api.PlayerDestroyPipeEvent;
+import de.robotricker.transportpipes.pipes.PipeUtils;
 import de.robotricker.transportpipes.pipeutils.ContainerBlockUtils;
 import net.md_5.bungee.api.ChatColor;
 
@@ -22,6 +26,7 @@ public class LootStorage {
 
 	//Add item to chests
 	public LootStorage(Collection<ItemStack> drops, Player player) {
+		
 		Chest chest1 = (Chest) firstChest.get(player.getUniqueId());
 		Chest chest2 = (Chest) secondChest.get(player.getUniqueId());
 		
@@ -29,10 +34,9 @@ public class LootStorage {
 			if (chest1 != null && chest2 != null) {
 				if (chest1.getLocation().getBlock().getType() == Material.CHEST && 
 						chest2.getLocation().getBlock().getType() == Material.CHEST) {
-						
+					//chests are valid
 					if (isChestNotFull(itemStack, chest1)) {
 						chest1.getBlockInventory().addItem(itemStack);
-
 						continue;
 					} else {
 						if (isChestNotFull(itemStack, chest2)) {
@@ -66,25 +70,24 @@ public class LootStorage {
 	//Place chests
 	public LootStorage(Block block, BlockFace blockFace, Player player, boolean vertical) {
 		if (vertical) {
+			Location loc1 = null;
+			Location loc2 = null;
+			
 			Block chestBlock1 = block.getRelative(BlockFace.UP);
 			
-			chestBlock1 = block.getRelative(BlockFace.SOUTH).getRelative(BlockFace.SOUTH);
+			loc1 = chestBlock1.getLocation().add(0, 2, 2);
+			loc2 = loc1.getBlock().getRelative(BlockFace.EAST).getLocation();
 			
-			Block chestBlock2 = chestBlock1.getRelative(BlockFace.EAST);
+			loc1.getBlock().setType(Material.CHEST);
+			loc2.getBlock().setType(Material.CHEST);
 			
-			chestBlock1.setType(Material.CHEST);
-			chestBlock2.setType(Material.CHEST);
-			
-			Chest chest1 = (Chest) chestBlock1.getState();
-			Chest chest2 = (Chest) chestBlock2.getState();
-			
-			chest1.getBlock().getState().update(true, false);
-			chest2.getBlock().getState().update(true, false);
-			
+			Chest chest1 = (Chest) loc1.getBlock().getState();
+			Chest chest2 = (Chest) loc2.getBlock().getState();
+
 			//Compatibility with TransportPipes: sync chest blocks with TransportPipes
 			if(Bukkit.getServer().getPluginManager().getPlugin("TransportPipes") != null) {
-				ContainerBlockUtils.updatePipeNeighborBlockSync(chestBlock1, true);
-				ContainerBlockUtils.updatePipeNeighborBlockSync(chestBlock2, true);
+				ContainerBlockUtils.updatePipeNeighborBlockSync(loc1.getBlock(), true);
+				ContainerBlockUtils.updatePipeNeighborBlockSync(loc2.getBlock(), true);
 			}
 			
 			firstChest.put(player.getUniqueId(), chest1);
@@ -93,14 +96,35 @@ public class LootStorage {
 			return;
 		}
 		
-		BlockFace face = blockFace.getOppositeFace();
+		BlockFace face = blockFace;
 		
-		Block chestBlock1 = block.getRelative(face);
+		Location loc1 = null;
+		Location loc2 = null;
 		
-		chestBlock1 = chestBlock1.getRelative(face);
-		chestBlock1 = chestBlock1.getRelative(BlockFace.DOWN);
+		switch (face) {
+		case NORTH:
+			loc1 = block.getLocation().add(0, 0, 3);
+			loc2 = loc1.getBlock().getRelative(BlockFace.EAST).getLocation();
+			break;
+		case EAST:
+			loc1 = block.getLocation().add(-3, 0, 0);
+			loc2 = loc1.getBlock().getRelative(BlockFace.SOUTH).getLocation();
+			break;
+		case SOUTH:
+			loc1 = block.getLocation().add(0, 0, -3);
+			loc2 = loc1.getBlock().getRelative(BlockFace.WEST).getLocation();
+			break;
+		case WEST:
+			loc1 = block.getLocation().add(3, 0, 0);
+			loc2 = loc1.getBlock().getRelative(BlockFace.NORTH).getLocation();
+			break;
+		default:
+			break;
+		}
 		
-		Block chestBlock2 = chestBlock1.getRelative(BlockFace.EAST);
+		Block chestBlock1 = loc1.getBlock();
+		
+		Block chestBlock2 = loc2.getBlock();
 		
 		chestBlock1.setType(Material.CHEST);
 		chestBlock2.setType(Material.CHEST);
@@ -136,4 +160,5 @@ public class LootStorage {
 	    boolean canContainitem = foundcount <= 0;
 		return canContainitem;
 	}
+	
 }
